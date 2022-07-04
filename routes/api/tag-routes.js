49fe21ece6db855ab860542df9f50de6e28376fe
rbenne30 <ryannebennett1@outlook.com>
2022-07-04 16:23:58 -0400

    update product route

--- routes/api/tag-routes.js
+++ routes/api/tag-routes.js
@@ -12,6 +12,28 @@ router.get('/', (req, res) => {
 });
 
 router.get('/:id', (req, res) => {
+    Tag.findOne({
+      where: {
+        id:req.params.id
+      },
+      include: [{
+        model: Product,
+      attributes: ['id', 'product_name', 'price', 'stock','category_id']
+      }
+      ]
+    })
+    .then(dbTagData => {
+      if (!dbTagData) {
+        res.status(404).json({message:'No tag found with this id'});
+        return
+      }
+      res.json(dbTagData);
+    })
+    .catch(err =>{
+      console.log(err);
+      res.status(500).json(err);
+    });
+    
 
   // find a single tag by its `id`
   // be sure to include its associated Product data
@@ -31,7 +53,7 @@ router.post('/seed', (req, res) => {
 // create a new tag
 
 router.put('/:tag:id', (req, res) => {
-  tag.update({
+  Tag.update({
     tag_id: req.body.tag_id,
   },
     {
